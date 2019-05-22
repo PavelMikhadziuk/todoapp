01cfe7ad5e3f43c34127e8c2229b48144078c84e
PavelMikhadziuk <pavel.mikhadziuk@startmatter.com>
2019-05-22 11:29:52 +0300

    Add simple using state for increasing font size

--- AwesomeProject/components/BlockText.js
+++ AwesomeProject/components/BlockText.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 export default class BlockText extends Component {
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = {
+      size: 8,
+    }
+  }
+
+
+  increaseSize = () => {
+    this.setState(prevState => ({ size: prevState.size + 1 }));
+  };
+
+  render() {
     /* otherwise, the text will flow as if it was one */
     return (
       <View>
         <Text>First part and </Text>
-        <Text>second part</Text>
+        <Text onLongPress={this.increaseSize} style={{ fontSize: this.state.size }}>second part</Text>
       </View>
     );
   }
