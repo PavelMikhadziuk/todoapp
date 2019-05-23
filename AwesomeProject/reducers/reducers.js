2cf4de43dd703895e9d0b22c9a344e58342a44b6
PavelMikhadziuk <pavel.mikhadziuk@startmatter.com>
2019-05-23 11:00:09 +0300

    Reimplement providing redux tools from scratch
    For understanding

--- AwesomeProject/reducers/reducers.js
+++ AwesomeProject/reducers/reducers.js
@@ -11,6 +11,28 @@ export const counter = (state = 0, action) => {
   }
 }
 
+const createStore = (reducer) => {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  }
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    }
+  }
+
+  dispatch({});
+
+  return { getState, dispatch, subscribe };
+}
 export const store = createStore(counter);
 console.log(store.getState());
 store.subscribe();
