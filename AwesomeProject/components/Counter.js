9a52350ae7036b5f5c9eed5dace1fb4aad4b8faa
PavelMikhadziuk <pavel.mikhadziuk@startmatter.com>
2019-05-26 23:00:23 +0300

    Add styles for button and counter text

--- AwesomeProject/components/Counter.js
+++ AwesomeProject/components/Counter.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import { Button, StyleSheet, View, Text } from 'react-native';
+import { TouchableNativeFeedback, StyleSheet, View, Text } from 'react-native';
 import { connect  } from 'react-redux';
 import { DECREMENT, INCREMENT } from '../constants/actions';
 
 const Counter = props => {
   return (
     <View style={styles.container}>
+      <View style={{ alignItems: 'center'}}>
+        <Text style={{ fontSize: 30 }}>Counter</Text>
+        <Text style={{ fontSize: 60 }}>{props.count}</Text>
+      </View>
       <View style={styles.alternativeLayoutButtonContainer}>
-        <Text>Counter</Text>
-        <Text>{props.count}</Text>
-        <Button
-          onPress={props.onIncrement}
-          title="+"
-        />
-        <Button
-          onPress={props.onDecrement}
-          title="-"
-        />
+        <TouchableNativeFeedback onPress={props.onIncrement}>
+          <View style={styles.buttonLayout}>
+            <Text style={styles.buttonTextLayout}>+</Text>
+          </View>
+        </TouchableNativeFeedback>
+        <TouchableNativeFeedback onPress={props.onDecrement}>
+          <View style={styles.buttonLayout}>
+            <Text style={styles.buttonTextLayout}>-</Text>
+          </View>
+        </TouchableNativeFeedback>
       </View>
     </View>
   );
@@ -24,13 +28,28 @@ const Counter = props => {
 
 const styles = StyleSheet.create({
   container: {
-   flex: 1,
-   justifyContent: 'center',
+    flex: 1,
+    justifyContent: 'center',
+    borderRadius: 4,
+    borderWidth: 5,
+    borderColor: 'green',
   },
   alternativeLayoutButtonContainer: {
     margin: 20,
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-around'
+  },
+  buttonLayout: {
+    paddingTop: 30,
+    paddingBottom: 30,
+    paddingLeft: 50,
+    paddingRight: 50,
+    backgroundColor: 'lightgreen',
+    borderRadius: 20
+  },
+  buttonTextLayout: {
+    fontSize: 30,
+    fontWeight: 'bold'
   }
 });
 
